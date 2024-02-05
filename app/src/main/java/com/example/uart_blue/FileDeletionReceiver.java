f7b03b5656798c7cad41c94932ab09c1b849c575
정진우 <91660287+jjw6712@users.noreply.github.com>
2024-02-05 19:38:08 +0900

    파일 저장기능 구현중

--- app/src/main/java/com/example/uart_blue/FileDeletionReceiver.java
+++ app/src/main/java/com/example/uart_blue/FileDeletionReceiver.java
@@ -1,10 +1,13 @@
 package com.example.uart_blue;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.documentfile.provider.DocumentFile;
 
@@ -14,23 +17,25 @@ public class FileDeletionReceiver extends BroadcastReceiver {
         // 디렉토리 URI를 SharedPreferences에서 가져옵니다.
         SharedPreferences prefs = context.getSharedPreferences("MySharedPrefs", Context.MODE_PRIVATE);
         String directoryUriStr = prefs.getString("directoryUri", "");
+        Log.d(TAG, "파일삭제 URI: "+ directoryUriStr);
         if (!directoryUriStr.isEmpty()) {
             Uri directoryUri = Uri.parse(directoryUriStr);
             // 파일 삭제 로직을 실행합니다.
-            deleteFilesInDirectory(context, directoryUri);
+            deleteWHDataDirectory(context, directoryUri);
         }
     }
 
-    private void deleteFilesInDirectory(Context context, Uri directoryUri) {
+    private void deleteWHDataDirectory(Context context, Uri directoryUri) {
         DocumentFile directory = DocumentFile.fromTreeUri(context, directoryUri);
         if (directory != null && directory.isDirectory()) {
-            for (DocumentFile file : directory.listFiles()) {
-                if (file.isFile() && file.getName().endsWith(".txt")) { // 텍스트 파일만 삭제
-                    file.delete();
-                }
+            DocumentFile whDataDirectory = directory.findFile("W.H.Data");
+            if (whDataDirectory != null && whDataDirectory.isDirectory()) {
+                // "W.H.Data" 폴더가 존재하면 폴더를 삭제합니다.
+                whDataDirectory.delete();
             }
         }
     }
 }
+
 
 
