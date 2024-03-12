c3c46799b1fd2c41863473072a0930a5941fbf27
user <jinwoo6712@gmail.com>
2024-03-12 23:19:58 +0900

    GPIOActivity 인스턴스 중복 생성 문제 해결

--- app/src/main/java/com/example/uart_blue/FileManager/FileProcessingTask.java
+++ app/src/main/java/com/example/uart_blue/FileManager/FileProcessingTask.java
@@ -1,7 +1,10 @@
 package com.example.uart_blue.FileManager;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -42,8 +45,24 @@ public class FileProcessingTask extends AsyncTask<Void, Void, Uri> {
                 FileManager.compressFile(context, combinedFileUri, outputZipUri); // 여기서 combinedFileUri는 .txt 파일을 가리킴
                 // 임시 .txt 파일 삭제
                 new File(combinedFileUri.getPath()).delete();
-                new TcpClient(outputZipUri, context, true, zipFileName).execute(); // 텍스트 파일 전송
-                Log.d(TAG, "서버로 압축파일 전송!!!!!!");
+                // 네트워크 연결 정보를 가져옵니다.
+                SharedPreferences sharedPreferences = context.getSharedPreferences("MySharedPrefs", MODE_PRIVATE);
+                String serverIP = sharedPreferences.getString("ServerIP", "");
+                String ZportStr = sharedPreferences.getString("ZPort", "");
+                String TPortStr = sharedPreferences.getString("TPort", "");
+                int Zport = ZportStr.isEmpty() ? -1 : Integer.parseInt(ZportStr);
+                int TPort = TPortStr.isEmpty() ? -1 : Integer.parseInt(TPortStr);
+
+
+                // 네트워크 연결 정보가 유효한 경우에만 TcpClient를 통해 파일 전송을 시도합니다.
+                if (!serverIP.isEmpty() || ZportStr.isEmpty()||TPortStr.isEmpty()) {
+                    new TcpClient(outputZipUri, context, true, zipFileName).execute();
+                    Log.d(TAG, "서버로 압축파일 전송!!!!!!");
+                } else {
+                    // 네트워크 연결 정보가 유효하지 않은 경우, 로컬에서의 처리를 로그로 남깁니다.
+                    Log.e(TAG, "네트워크 정보가 유효하지 않습니다. 로컬에서 파일 처리를 완료합니다.");
+                }
+
                 return outputZipUri;
             }
         }
